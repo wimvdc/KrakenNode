2b0d02498feb73e5da884b25ed4325782cdd202b
Wim Vande Casteele <wim@Wims-MacBook-Pro.local>
2017-01-22 00:05:48 +0100

    Heroku test

--- kraken.js
+++ kraken.js
@@ -3,7 +3,7 @@ var express = require('express')
   , app = express();
 var argv = require('minimist')(process.argv.slice(2));
 console.log(argv);
-console.lof(process.env);
+console.log(process.env);
 var kraken = new KrakenClient(process.env.KRAKEN_PUBLIC_KEY || argv.KRAKEN_API_KEY,process.env.KRAKEN_PUBLIC_KEY || argv.KRAKEN_PUBLIC_KEY);
 
 app.use(express.static(__dirname+'/public'));
